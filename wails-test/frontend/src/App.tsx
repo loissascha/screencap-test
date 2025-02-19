972ce1934aee633025b56c1944b67c6bdab71582
Sascha Loishandl <sascha.loishandl@gmail.com>
2025-02-19 14:29:24 +0100

    wails

--- wails-test/frontend/src/App.tsx
+++ wails-test/frontend/src/App.tsx
@@ -1,28 +1,45 @@
-import {useState} from 'react';
-import logo from './assets/images/logo-universal.png';
-import './App.css';
-import {Greet} from "../wailsjs/go/main/App";
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-    const [resultText, setResultText] = useState("Please enter your name below 👇");
-    const [name, setName] = useState('');
-    const updateName = (e: any) => setName(e.target.value);
-    const updateResultText = (result: string) => setResultText(result);
+    const [logs, setLogs] = useState<string>("");
+    useEffect(() => {
+        // Check if RTCPeerConnection is available
+        if (typeof RTCPeerConnection !== "undefined") {
+            // webrtc peer
+            const peerConnection = new RTCPeerConnection({
+                iceServers: [
+                    { urls: "stun:stun.l.google.com:19302" }, // Use a public STUN server
+                ],
+            });
+        } else {
+            console.error("RTCPeerConnection is not available.");
+            setLogs("WEBRTC not available!");
+        }
 
-    function greet() {
-        Greet(name).then(updateResultText);
-    }
+        // socket
+        const socket = new WebSocket("ws://sl-dev.at:8998");
+    }, []);
 
     return (
         <div id="App">
-            <img src={logo} id="logo" alt="logo"/>
-            <div id="result" className="result">{resultText}</div>
-            <div id="input" className="input-box">
-                <input id="name" className="input" onChange={updateName} autoComplete="off" name="input" type="text"/>
-                <button className="btn" onClick={greet}>Greet</button>
-            </div>
+            <p>
+                This example shows you the contents of the selected part of your
+                display. Click the Start Capture button to begin.
+            </p>
+
+            <p>
+                <button id="start">Start Capture</button>&nbsp;
+                <button id="stop">Stop Capture</button>
+            </p>
+
+            <video id="video" autoPlay></video>
+            <br />
+
+            <strong>Log:</strong>
+            {logs}
         </div>
-    )
+    );
 }
 
-export default App
+export default App;
